58d5a433985f3caba356110168782151c28ef426
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 15:02:56 +0100

    Update .git-zsh-for-mac.sh

--- .git-zsh-for-mac.sh
+++ .git-zsh-for-mac.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source ~/.git-completion.zsh
-source ~/.git-prompt.sh
+source ~/.zsh/git-completion.zsh
+source ~/.zsh/git-prompt.sh
 
 DEFAULT="\[\033[0;00m\]"
 GREEN="\[\033[0;32m\]"
 YELLOW="\[\033[0;33m\]"
 CYAN="\[\033[0;36m\]"
 
-export PS1=$GREEN"\u@\h"$YELLOW" \w"$CYAN'$(__git_ps1 " (%s)")'$DEFAULT"\n\$ "
+setopt PROMPT_SUBST ; PS1=$GREEN"%n@%m"$YELLOW" %c"$CYAN'$(__git_ps1 " (%s)")'$DEFAULT"\n\$ "
