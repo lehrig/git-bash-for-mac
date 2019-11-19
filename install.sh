65ac352c68a13946328ade2647ccd7e973131542
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 14:53:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
 
-curl -o ~/.git-zsh-for-mac.sh https://raw.githubusercontent.com/lehrig/git-zsh-for-mac/master/.git-zsh-for-mac.sh
-curl -o ~/.git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
-curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+mkdir -p ~/.zsh
+
+curl -o ~/.zsh/git-zsh-for-mac.sh https://raw.githubusercontent.com/lehrig/git-zsh-for-mac/master/.git-zsh-for-mac.sh
+curl -o ~/.zsh/git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 # Add newline at the end of '.zshrc' if it doesn't exist
 sed -i '' '$a\' ~/.zshrc
 
-echo "source ~/.git-zsh-for-mac.sh" >> ~/.zshrc
+echo "source ~/.zsh/git-zsh-for-mac.sh" >> ~/.zshrc
