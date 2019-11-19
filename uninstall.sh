468b95423bd0792a725521afd6ca07fca51affb5
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 14:55:15 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm ~/.git-zsh-for-mac.sh
-rm ~/.git-completion.zsh
-rm ~/.git-prompt.sh
+rm ~/.zsh/git-zsh-for-mac.sh
+rm ~/.zsh/git-completion.zsh
+rm ~/.zsh/git-prompt.sh
 
-sed -i.backup '/source ~\/.git-zsh-for-mac.sh/d' ~/.zshrc 
+sed -i.backup '/source ~\/.zsh\/git-zsh-for-mac.sh/d' ~/.zshrc 
