f1a61834631eb44c4a89cec40ae8fde11a616178
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 15:19:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 mkdir -p ~/.zsh
 
 curl -o ~/.zsh/git-zsh-for-mac.sh https://raw.githubusercontent.com/lehrig/git-zsh-for-mac/master/.git-zsh-for-mac.sh
+curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o ~/.zsh/git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
