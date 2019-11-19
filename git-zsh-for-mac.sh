90efb1fd408f461fc4f15b582a186db5a4ab6aa3
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 17:15:47 +0100

    Update git-zsh-for-mac.sh

--- git-zsh-for-mac.sh
+++ git-zsh-for-mac.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-# see https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-# deprectated; see https://stackoverflow.com/questions/28028740/git-tab-completion-in-zsh-throwing-errors
-# source ~/.zsh/git-completion.bash
-
 # see https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+# see https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+# see see https://stackoverflow.com/questions/28028740/git-tab-completion-in-zsh-throwing-errors
 zstyle ':completion:*:*:git:*' script ~/.zsh/git-completion.bash
 fpath=(~/.zsh $fpath)
 
 # see https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 source ~/.zsh/git-prompt.sh
 
+# color support for prompt
+# see https://stackoverflow.com/questions/689765/how-can-i-change-the-color-of-my-prompt-in-zsh-different-from-normal-text
+autoload -U colors && colors
+DEFAULT="%{$fg[black]%}"
+GREEN="%{$fg[green]%}"
+YELLOW="%{$fg[yellow]%}"
+CYAN="%{$fg[cyan]%}"
+
+# prompt format
 # see https://wiki.gentoo.org/wiki/Zsh/Guide
 USERNAME='%n'
 MACHINE='%m'
 WORKDIR='%d'
 NEWLINE=$'\n'
 GITBRANCH=$(__git_ps1 " (%s)")
-setopt PROMPT_SUBST ; PS1='${USERNAME}@${MACHINE} ${WORKDIR}${GITBRANCH}${NEWLINE}\$ '
 
-# color support (to be replaced by https://github.com/Powerlevel9k/powerlevel9k)
-#DEFAULT="\[\033[0;00m\]"
-#GREEN="\[\033[0;32m\]"
-#YELLOW="\[\033[0;33m\]"
-#CYAN="\[\033[0;36m\]"
-#setopt PROMPT_SUBST ; PS1=$GREEN"%n@%m"$YELLOW" %c"$CYAN'$(__git_ps1 " (%s)")'$DEFAULT"\n\$ "
+setopt PROMPT_SUBST ; PS1='${GREEN}${USERNAME}@${MACHINE}${YELLOW} ${WORKDIR}${CYAN}${GITBRANCH}${DEFAULT}${NEWLINE}\$ '
 
 # reload
 autoload -Uz compinit && compinit
-
