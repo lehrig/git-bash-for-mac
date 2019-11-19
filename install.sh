9c93c27160bf12dbb29a609f20dcca53a5e0e9d9
Dr. Sebastian Lehrig <github@lehrig.de>
2019-11-19 17:02:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.zsh
 
 curl -o ~/.zsh/git-zsh-for-mac.sh https://raw.githubusercontent.com/lehrig/git-zsh-for-mac/master/git-zsh-for-mac.sh
 curl -o ~/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o ~/.zsh/git-completion.zsh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -o ~/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 # Add newline at the end of '.zshrc' if it doesn't exist
